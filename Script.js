246955849f06c5f5cf34ec497ec6239b8e153801
MinerProjects <155327531+MinerProjects@users.noreply.github.com>
2024-01-01 01:51:16 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,223 @@
+var x = prompt("Please Enter Your Name   ( example- Anuhas )");
+
+if (x == "Anuhas")
+{
+    alert("Hey Its me. It still working don't worry")
+}
+
+else if (x == "Chintha")
+{
+    alert("Hi Ammi. I Wish you a Happy Happy new year")
+    alert("and also i wish u for your oparation")
+    alert("lov u <3 Kurulu")
+}
+else if (x == "chintha")
+{
+    alert("Hi Ammi. I Wish you a Happy Happy new year")
+    alert("and also i wish u for your oparation")
+    alert("lov u <3 Kurulu")
+}
+
+else if (x == "Upul")
+{
+    alert("Hi Appachchi. I Wish you a Happy Happy new year")
+    alert("and also I wish u to travel safely to your dream country")
+    alert("lov u <3 Anu")
+}
+else if (x == "upul")
+{
+    alert("Hi Appachchi. I Wish you a Happy Happy new year")
+    alert("and also I wish u to travel safely to your dream country")
+    alert("lov u <3 Anu")
+}
+
+else if (x == "Chanu")
+{
+    alert("Hi Chanu. I Wish you a Happy Happy new year")
+    alert("and also I wish u to Win a lottery")
+    alert("lov u <3 Anu Ayya")
+}
+else if (x == "chanu")
+{
+    alert("Hi Chanu. I Wish you a Happy Happy new year")
+    alert("and also I wish u to Win a lottery")
+    alert("lov u <3 Anu Ayya")
+}
+
+else if (x == "Manu")
+{
+    alert("Hi Manu. I Wish you a Happy Happy new year")
+    alert("and also I wish u to cure ur backbone")
+    alert("lov u <3 Anu Ayya")
+}
+else if (x == "manu")
+{
+    alert("Hi Manu. I Wish you a Happy Happy new year")
+    alert("and also I wish u to cure ur backbone")
+    alert("lov u <3 Anu Ayya")
+}
+
+else if (x == "Lalindi")
+{
+    alert("Hi Lalindi. I Wish you a Happy Happy new year")
+    alert("and also I wish u to make ur dreams come true this year")
+    alert("lov u <3 Anu Ayya")
+}
+else if (x == "Hansaki")
+{
+    alert("Hi Lalindi. I Wish you a Happy Happy new year")
+    alert("and also I wish u to make ur dreams come true this year")
+    alert("lov u <3 Anu Ayya")
+}
+else if (x == "hansaki")
+{
+    alert("Hi Lalindi. I Wish you a Happy Happy new year")
+    alert("and also I wish u to make ur dreams come true this year")
+    alert("lov u <3 Anu Ayya")
+}
+else if (x == "lalindi")
+{
+    alert("Hi Lalindi. I Wish you a Happy Happy new year")
+    alert("and also I wish u to make ur dreams come true this year")
+    alert("lov u <3 Anu Ayya")
+}
+
+else if (x == "Bimani")
+{
+    alert("Hi Hansi akka. I Wish you a Happy Happy new year")
+    alert("and also I wish u to make ur dreams come true this year")
+    alert("lov u <3 Anu Malli")
+}else if (x == "bimani")
+{
+    alert("Hi Hansi akka. I Wish you a Happy Happy new year")
+    alert("and also I wish u to make ur dreams come true this year")
+    alert("lov u <3 Anu Malli")
+}
+else if (x == "Hansi")
+{
+    alert("Hi Hansi akka. I Wish you a Happy Happy new year")
+    alert("and also I wish u to make ur dreams come true this year")
+    alert("lov u <3 Anu Malli")
+}
+else if (x == "hansi")
+{
+    alert("Hi Hansi akka. I Wish you a Happy Happy new year")
+    alert("and also I wish u to make ur dreams come true this year")
+    alert("lov u <3 Anu Malli")
+}
+
+else if (x == "Nanda")
+{
+    alert("Hi Ammamma. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("lov u <3 Anuhas putha")
+}
+else if (x == "nanda")
+{
+    alert("Hi Ammamma. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("lov u <3 Anuhas putha")
+}
+else if (x == "Ammamma")
+{
+    alert("Hi Ammamma. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("lov u <3 Anuhas putha")
+}
+else if (x == "ammamma")
+{
+    alert("Hi Ammamma. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("lov u <3 Anuhas putha")
+}
+
+else if (x == "Rikula")
+{
+    alert("Hallo Minecraft Youtuber")
+    alert("Happy new year Hode")
+    alert("<3 Anuhas")
+    alert("ee vunata uba thama nether pothel ekavath hadala nah :)")
+}
+else if (x == "rikula")
+{
+    alert("Hallo Minecraft Youtuber")
+    alert("Happy new year Hode")
+    alert("<3 Anuhas")
+    alert("ee vunata uba thama nether pothel ekavath hadala nah :)")
+}
+
+else if (x == "Temiya")
+{
+    alert("Hallo bandaravela bokka")
+    alert("Happy new year Hode")
+    alert("<3 Anuhas")
+    alert("veren mege ikmanata")
+}
+else if (x == "temiya")
+{
+    alert("Hallo bandaravela bokka")
+    alert("Happy new year Hode")
+    alert("<3 Anuhas")
+    alert("veren mege ikmanata")
+}
+
+else if (x == "Yasindu")
+{
+    alert("Hallo my bokka")
+    alert("Happy new year Hode")
+    alert("3 veni vareta gemak gahamu kollo")
+    alert("<3 Anuhas")
+}
+else if (x == "yasindu")
+{
+    alert("Hallo my bokka")
+    alert("Happy new year Hode")
+    alert("3 veni vareta gemak gahamu kollo")
+    alert("<3 Anuhas")
+}
+
+else if (x == "Rajini")
+{
+    alert("Hi Muthu ammaa. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("lov u <3 Anuhas putha")
+}
+else if (x == "rajini")
+{
+    alert("Hi Muthu ammaa. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("lov u <3 Anuhas putha")
+}
+
+else if (x == "Supun")
+{
+    alert("Hi Supun bappe. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("lov u <3 Anuhas")
+}
+else if (x == "supun")
+{
+    alert("Hi Supun bappe. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("lov u <3 Anuhas")
+}
+
+else if (x == "Hasini")
+{
+    alert("Hi Hasini nande. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("<3 Anuhas")
+}
+else if (x == "hasini")
+{
+    alert("Hi Hasini nande. I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life in this year")
+    alert("<3 Anuhas")
+}
+
+else
+{
+    alert("Oh Hi "+x+" .I Wish you a Happy Happy new year")
+    alert("and also I wish u to a helthy life and all ur dreams comes true in this year")
+    alert("Wish you - Anuhas")
+}
